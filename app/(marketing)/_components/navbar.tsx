f90826ce2187f6b83afebfee1a9022e89de43cd4
LostoVayne <moises.barrera.dev@gmail.com>
2023-11-26 20:31:07 -0300

    Agregado sistema de autenticacion usando Clerk

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -4,8 +4,15 @@ import { ModeToggle } from "@/components/mode-toggle";
 import { useScrollTop } from "@/hooks/use-scroll-top";
 import { cn } from "@/lib/utils";
 import { Logo } from "./logo";
+import { useConvexAuth } from "convex/react";
+import { SignInButton, UserButton } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/spinner";
+import Link from "next/link";
 
 const Navbar = () => {
+    const { isAuthenticated, isLoading } = useConvexAuth();
+
     const scrolled = useScrollTop();
 
     return (
@@ -17,6 +24,29 @@ const Navbar = () => {
         >
             <Logo />
             <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
+                {isLoading && <Spinner />}
+                {!isAuthenticated && !isLoading && (
+                    <>
+                        <SignInButton mode="modal">
+                            <Button variant="ghost" size="sm">
+                                Log in
+                            </Button>
+                        </SignInButton>
+                        <SignInButton mode="modal">
+                            <Button size="sm">Get Jotion Free</Button>
+                        </SignInButton>
+                    </>
+                )}
+
+                {isAuthenticated && !isLoading && (
+                    <>
+                        <Button variant="ghost" size="sm" asChild>
+                            <Link href="/documents">Enter Jotion</Link>
+                        </Button>
+                        <UserButton afterSignOutUrl="/" />
+                    </>
+                )}
+
                 <ModeToggle />
             </div>
         </div>
