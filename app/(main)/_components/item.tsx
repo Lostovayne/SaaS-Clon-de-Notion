6e31a03fb3ad5963dd6631fe333ba157c51604aa
Lostovayne <moises.barrera.dev@gmail.com>
2024-10-04 23:20:28 -0300

    agrega trash a los documents

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -1,11 +1,19 @@
 "use client";
 
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+} from "@/components/ui/dropdown-menu";
 import { Skeleton } from "@/components/ui/skeleton";
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
 import { cn } from "@/lib/utils";
+import { useUser } from "@clerk/clerk-react";
 import { useMutation } from "convex/react";
-import { ChevronDown, ChevronRight, LucideIcon, Plus } from "lucide-react";
+import { ChevronDown, ChevronRight, LucideIcon, MoreHorizontal, Plus, Trash } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { toast } from "sonner";
 
@@ -34,6 +42,7 @@ export const Item = ({
   level = 0,
   onExpand,
 }: ItemProps) => {
+  const { user } = useUser();
   const router = useRouter();
   const create = useMutation(api.documents.create);
 
@@ -67,7 +76,7 @@ export const Item = ({
   return (
     <div
       onClick={onClick}
-      role="button"
+      role='button'
       style={{ paddingLeft: level ? `${level * 12 + 12}px` : "12px" }}
       className={cn(
         "group min-h-[27px] text-sm py-1 pr-3 w-full hover:bg-primary/5 flex items-center text-muted-foreground font-medium",
@@ -75,33 +84,52 @@ export const Item = ({
       )}>
       {!!id && (
         <div
-          className="h-full rounded-md hover:bg-neutral-300 dark:bg-neutral-600 mr-1"
-          role="button"
+          className='h-full rounded-md hover:bg-neutral-300 dark:bg-neutral-600 mr-1'
+          role='button'
           onClick={handleExpand}>
           <ChevronIcon />
         </div>
       )}
 
-      {documentIcon ?
-        <div className="shrink-0 mr-2 text-[18px]">{documentIcon}</div>
-      : <Icon className="shrink-0 h-[18px] mr-2 text-muted-foreground" />}
+      {documentIcon ? (
+        <div className='shrink-0 mr-2 text-[18px]'>{documentIcon}</div>
+      ) : (
+        <Icon className='shrink-0 h-[18px] mr-2 text-muted-foreground' />
+      )}
 
-      <span className="truncate">{label}</span>
+      <span className='truncate'>{label}</span>
       {isSearch && (
         <kbd
           className={
             "ml-auto pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium  text-muted-foreground opacity-100"
           }>
-          <span className="text-xs">⌘</span>K
+          <span className='text-xs'>⌘</span>K
         </kbd>
       )}
       {!!id && (
-        <div className="ml-auto flex items-center gap-x-2">
+        <div className='ml-auto flex items-center gap-x-2'>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild onClick={(e) => e.stopPropagation()}>
+              <div
+                role='button'
+                className='opacity-0 group-hover:opacity-100 h-full ml-auto rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600'>
+                <MoreHorizontal className='size-4 text-muted-foreground' />
+              </div>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className='w-60' align='start' side='right' forceMount>
+              <DropdownMenuItem onClick={() => {}} className='text-muted-foreground'>
+                <Trash className='size-4  mr-2' />
+                Delete
+              </DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <div className='text-xs text-muted-foreground p-2'>Last edited by: {user?.fullName}</div>
+            </DropdownMenuContent>
+          </DropdownMenu>
           <div
-            role="button"
+            role='button'
             onClick={onCreate}
-            className=" opacity-0 group-hover:opacity-100  h-full ml-auto rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600 mr-1">
-            <Plus className="size-4 text-muted-foreground" />
+            className=' opacity-0 group-hover:opacity-100  h-full ml-auto rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600 mr-1'>
+            <Plus className='size-4 text-muted-foreground' />
           </div>
         </div>
       )}
@@ -115,9 +143,9 @@ Item.Skeleton = function ItemSkeleton({ level }: { level: number }) {
       style={{
         paddingLeft: level ? `${level * 12 + 25}px` : "12px",
       }}
-      className="flex gap-2 py-[3px]">
-      <Skeleton className="size-4" />
-      <Skeleton className="h-4 w-[30%]" />
+      className='flex gap-2 py-[3px]'>
+      <Skeleton className='size-4' />
+      <Skeleton className='h-4 w-[30%]' />
     </div>
   );
 };
--- convex/documents.ts
+++ convex/documents.ts
@@ -16,6 +16,52 @@ export const get = query({
   },
 });
 
+export const archive = mutation({
+  args: {
+    id: v.id("documents"),
+  },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new Error("Not authenticated");
+    }
+
+    const userId = identity.subject;
+    const existingDocument = await ctx.db.get(args.id);
+
+    if (!existingDocument) {
+      throw new Error("Document not found");
+    }
+
+    if (existingDocument.userId !== userId) {
+      throw new Error("You are not authorized to archive this document");
+    }
+
+    // Recursive function
+    const recursiveArchive = async (documentId: Id<"documents">) => {
+      const children = await ctx.db
+        .query("documents")
+        .withIndex("by_user_parent", (q) => q.eq("userId", userId).eq("parentDocument", documentId))
+        .collect();
+
+      for (const child of children) {
+        await ctx.db.patch(child._id, {
+          isArchived: true,
+        });
+        await recursiveArchive(child._id);
+      }
+    };
+
+    const document = await ctx.db.patch(args.id, {
+      isArchived: true,
+    });
+
+    recursiveArchive(args.id);
+    return document;
+  },
+});
+
 export const getSidebar = query({
   args: {
     parentDocument: v.optional(v.id("documents")),
