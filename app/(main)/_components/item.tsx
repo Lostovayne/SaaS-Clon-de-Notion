f6847f529358bb84ebe6bf6c0a580e410f3d9c4b
Lostovayne <moises.barrera.dev@gmail.com>
2024-10-13 23:54:33 -0300

    agrega la funcionalidad de eliminar

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -45,6 +45,20 @@ export const Item = ({
   const { user } = useUser();
   const router = useRouter();
   const create = useMutation(api.documents.create);
+  const archive = useMutation(api.documents.archive);
+
+  const onArchive = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    event.stopPropagation();
+    if (!id) return;
+    const promise = archive({ id });
+    console.log(promise);
+
+    toast.promise(promise, {
+      loading: "Moving to trash...",
+      success: "Document moved to trash!",
+      error: "Error moving the document to trash.",
+    });
+  };
 
   // HandleExpand
   const handleExpand = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
@@ -101,9 +115,9 @@ export const Item = ({
       {isSearch && (
         <kbd
           className={
-            "ml-auto pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium  text-muted-foreground opacity-100"
+            "ml-auto pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-xs font-medium  text-muted-foreground opacity-100"
           }>
-          <span className='text-xs'>⌘</span>K
+          <span className='text-sm'>⌘</span>K
         </kbd>
       )}
       {!!id && (
@@ -117,7 +131,7 @@ export const Item = ({
               </div>
             </DropdownMenuTrigger>
             <DropdownMenuContent className='w-60' align='start' side='right' forceMount>
-              <DropdownMenuItem onClick={() => {}} className='text-muted-foreground'>
+              <DropdownMenuItem onClick={onArchive} className='text-muted-foreground'>
                 <Trash className='size-4  mr-2' />
                 Delete
               </DropdownMenuItem>
