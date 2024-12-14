0543f835b7d507c3cfdeadd1a5ed2ad1a1c80609
Lostovayne <moises.barrera.dev@gmail.com>
2024-12-14 17:48:24 -0300

    fix: url -Documents error

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,186 +1,181 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-"use client";
-import { api } from "@/convex/_generated/api";
-import { useSearch } from "@/hooks/use-search";
-import { useSettings } from "@/hooks/use-settings";
-import { cn } from "@/lib/utils";
-import { useMutation } from "convex/react";
-import { ChevronsLeft, MenuIcon, Plus, PlusCircle, Search, Settings, Trash } from "lucide-react";
-import { useParams, usePathname } from "next/navigation";
-import { ElementRef, useEffect, useRef, useState } from "react";
-import { toast } from "sonner";
-import { useMediaQuery } from "usehooks-ts";
-import DocumentList from "./document-list";
-import { Item } from "./item";
-import Navbar from "./navbar";
-import UserItem from "./user-item";
+'use client';
+import { api } from '@/convex/_generated/api';
+import { useSearch } from '@/hooks/use-search';
+import { useSettings } from '@/hooks/use-settings';
+import { cn } from '@/lib/utils';
+import { useMutation } from 'convex/react';
+import { ChevronsLeft, MenuIcon, Plus, PlusCircle, Search, Settings, Trash } from 'lucide-react';
+import { useParams, usePathname } from 'next/navigation';
+import { ElementRef, useEffect, useRef, useState } from 'react';
+import { toast } from 'sonner';
+import { useMediaQuery } from 'usehooks-ts';
+import DocumentList from './document-list';
+import { Item } from './item';
+import Navbar from './navbar';
+import UserItem from './user-item';
 
 //Components Shadcn
-import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
-import TrashBox from "./trash-box";
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
+import TrashBox from './trash-box';
 
 export const Navigation = () => {
-  // hooks
-  const settings = useSettings();
-  const search = useSearch();
-  const pathname = usePathname();
-  const params = useParams();
-  // npm i usehooks-ts
-  const isMobile = useMediaQuery("(max-width:768px)");
+	// hooks
+	const settings = useSettings();
+	const search = useSearch();
+	const pathname = usePathname();
+	const params = useParams();
+	// npm i usehooks-ts
+	const isMobile = useMediaQuery('(max-width:768px)');
 
-  // Leer los documentos desde convex
-  const create = useMutation(api.documents.create);
+	// Leer los documentos desde convex
+	const create = useMutation(api.documents.create);
 
-  const isResizingRef = useRef(false);
-  const sidebarRef = useRef<ElementRef<"aside">>(null);
-  const navbarRef = useRef<ElementRef<"div">>(null);
-  // states
-  const [isResetting, setIsResetting] = useState(false);
-  const [isCollapsed, setIsCollapsed] = useState(isMobile);
+	const isResizingRef = useRef(false);
+	const sidebarRef = useRef<ElementRef<'aside'>>(null);
+	const navbarRef = useRef<ElementRef<'div'>>(null);
+	// states
+	const [isResetting, setIsResetting] = useState(false);
+	const [isCollapsed, setIsCollapsed] = useState(isMobile);
 
-  useEffect(() => {
-    if (isMobile) {
-      collapse();
-    } else {
-      resetWidth();
-    }
-  }, [isMobile]);
+	useEffect(() => {
+		if (isMobile) {
+			collapse();
+		} else {
+			resetWidth();
+		}
+	}, [isMobile]);
 
-  useEffect(() => {
-    if (isMobile) {
-      collapse();
-    }
-  }, [pathname, isMobile]);
+	useEffect(() => {
+		if (isMobile) {
+			collapse();
+		}
+	}, [pathname, isMobile]);
 
-  //funciones
-  const handleMouseDown = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-    event.preventDefault();
-    event.stopPropagation();
-    isResizingRef.current = true;
-    document.addEventListener("mousemove", handleMouseMove);
-    document.addEventListener("mouseup", handleMouseUp);
-  };
+	//funciones
+	const handleMouseDown = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+		event.preventDefault();
+		event.stopPropagation();
+		isResizingRef.current = true;
+		document.addEventListener('mousemove', handleMouseMove);
+		document.addEventListener('mouseup', handleMouseUp);
+	};
 
-  const handleMouseMove = (event: MouseEvent) => {
-    if (!isResizingRef.current) return;
-    let newWidth = event.clientX;
-    if (newWidth < 240) newWidth = 240;
-    if (newWidth > 480) newWidth = 480;
-    if (sidebarRef.current && navbarRef.current) {
-      sidebarRef.current.style.width = `${newWidth}px`;
-      navbarRef.current.style.setProperty("left", `${newWidth}px`);
-      navbarRef.current.style.setProperty("width", `calc(100% - ${newWidth}px)`);
-    }
-  };
+	const handleMouseMove = (event: MouseEvent) => {
+		if (!isResizingRef.current) return;
+		let newWidth = event.clientX;
+		if (newWidth < 240) newWidth = 240;
+		if (newWidth > 480) newWidth = 480;
+		if (sidebarRef.current && navbarRef.current) {
+			sidebarRef.current.style.width = `${newWidth}px`;
+			navbarRef.current.style.setProperty('left', `${newWidth}px`);
+			navbarRef.current.style.setProperty('width', `calc(100% - ${newWidth}px)`);
+		}
+	};
 
-  const handleMouseUp = () => {
-    isResizingRef.current = false;
-    document.removeEventListener("mousemove", handleMouseMove);
-    document.removeEventListener("mouseup", handleMouseUp);
-  };
+	const handleMouseUp = () => {
+		isResizingRef.current = false;
+		document.removeEventListener('mousemove', handleMouseMove);
+		document.removeEventListener('mouseup', handleMouseUp);
+	};
 
-  const resetWidth = () => {
-    if (sidebarRef.current && navbarRef.current) {
-      setIsCollapsed(false);
-      setIsResetting(true);
+	const resetWidth = () => {
+		if (sidebarRef.current && navbarRef.current) {
+			setIsCollapsed(false);
+			setIsResetting(true);
 
-      sidebarRef.current.style.width = isMobile ? "100%" : "240px";
-      navbarRef.current.style.setProperty("width", isMobile ? "0" : "calc(100% - 240px)");
-      navbarRef.current.style.setProperty("left", isMobile ? "100%" : "240px");
-    }
+			sidebarRef.current.style.width = isMobile ? '100%' : '240px';
+			navbarRef.current.style.setProperty('width', isMobile ? '0' : 'calc(100% - 240px)');
+			navbarRef.current.style.setProperty('left', isMobile ? '100%' : '240px');
+		}
 
-    setTimeout(() => {
-      setIsResetting(false);
-    }, 300);
-  };
+		setTimeout(() => {
+			setIsResetting(false);
+		}, 300);
+	};
 
-  const collapse = () => {
-    if (sidebarRef.current && navbarRef.current) {
-      setIsCollapsed(true);
-      setIsResetting(true);
+	const collapse = () => {
+		if (sidebarRef.current && navbarRef.current) {
+			setIsCollapsed(true);
+			setIsResetting(true);
 
-      sidebarRef.current.style.width = "0";
-      navbarRef.current.style.setProperty("width", "100%");
-      navbarRef.current.style.setProperty("left", "0");
+			sidebarRef.current.style.width = '0';
+			navbarRef.current.style.setProperty('width', '100%');
+			navbarRef.current.style.setProperty('left', '0');
 
-      setTimeout(() => {
-        setIsResetting(false);
-      }, 300);
-    }
-  };
+			setTimeout(() => {
+				setIsResetting(false);
+			}, 300);
+		}
+	};
 
-  const handleCreate = () => {
-    const promise = create({ title: "Untitled" });
+	const handleCreate = () => {
+		const promise = create({ title: 'Untitled' });
 
-    toast.promise(promise, {
-      loading: "Creating a new note...",
-      success: "Note created successfully!",
-      error: "Error creating the note.",
-    });
-  };
+		toast.promise(promise, {
+			loading: 'Creating a new note...',
+			success: 'Note created successfully!',
+			error: 'Error creating the note.'
+		});
+	};
 
-  return (
-    <>
-      <aside
-        ref={sidebarRef}
-        className={cn(
-          "group/sidebar h-full bg-secondary overflow-y-auto  relative flex w-60 flex-col z-[99999]",
-          isResetting && "transition-all ease-in-out duration-300",
-          isMobile && "w-0"
-        )}
-      >
-        <div
-          onClick={collapse}
-          role="button"
-          className={cn(
-            "h-6 w-6 text-muted-foreground rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600 absolute top-3 right-2 opacity-0 group-hover/sidebar:opacity-100 transition cursor-pointer",
-            isMobile && "opacity-100"
-          )}
-        >
-          <ChevronsLeft className="w-6 h-6" />
-        </div>
-        <div>
-          {/* Acciones del usuario */}
-          <UserItem />
-          <Item label="Search" isSearch icon={Search} onClick={search.onOpen} />{" "}
-          <Item label="Settings" icon={Settings} onClick={settings.onOpen} />
-          <Item onClick={handleCreate} label="New page" icon={PlusCircle} />
-        </div>
-        <div className="mt-4">
-          <DocumentList />
-          <Item onClick={handleCreate} icon={Plus} label="Add a page" />
-          <Popover>
-            <PopoverTrigger className="w-full mt-4">
-              <Item icon={Trash} label="Trash" onClick={() => {}} />
-            </PopoverTrigger>
-            <PopoverContent side={isMobile ? "bottom" : "right"} className="p-0 w-72">
-              <TrashBox />
-            </PopoverContent>
-          </Popover>
-        </div>
-        <div
-          onMouseDown={handleMouseDown}
-          onClick={resetWidth}
-          className="top-0 right-0 absolute bg-primary/10 opacity-0 group-hover/sidebar:opacity-100 w-1 h-full transition cursor-ew-resize"
-        />
-      </aside>
-      <div
-        ref={navbarRef}
-        className={cn(
-          "absolute top-0 z-[99999] left-60 w-[calc(100%-240px)]",
-          isResetting && "transition-all ease-in-out duration-300",
-          isMobile && "left-0 W-FULL"
-        )}
-      >
-        {!!params.documentId ?
-          <Navbar isCollapsed={isCollapsed} onResetWidth={resetWidth} />
-        : <nav className="bg-transparent px-3 py-2 w-full">
-            {isCollapsed && (
-              <MenuIcon role="button" onClick={resetWidth} className="w-6 h-6 text-muted-foreground" />
-            )}
-          </nav>
-        }
-      </div>
-    </>
-  );
+	return (
+		<>
+			<aside
+				ref={sidebarRef}
+				className={cn(
+					'group/sidebar h-full bg-secondary overflow-y-auto  relative flex w-60 flex-col z-[99999]',
+					isResetting && 'transition-all ease-in-out duration-300',
+					isMobile && 'w-0'
+				)}>
+				<div
+					onClick={collapse}
+					role="button"
+					className={cn(
+						'h-6 w-6 text-muted-foreground rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600 absolute top-3 right-2 opacity-0 group-hover/sidebar:opacity-100 transition cursor-pointer',
+						isMobile && 'opacity-100'
+					)}>
+					<ChevronsLeft className="w-6 h-6" />
+				</div>
+				<div>
+					{/* Acciones del usuario */}
+					<UserItem />
+					<Item label="Search" isSearch icon={Search} onClick={search.onOpen} />
+					<Item label="Settings" icon={Settings} onClick={settings.onOpen} />
+					<Item onClick={handleCreate} label="New page" icon={PlusCircle} />
+				</div>
+				<div className="mt-4">
+					<DocumentList />
+					<Item onClick={handleCreate} icon={Plus} label="Add a page" />
+					<Popover>
+						<PopoverTrigger className="w-full mt-4">
+							<Item icon={Trash} label="Trash" onClick={() => {}} />
+						</PopoverTrigger>
+						<PopoverContent side={isMobile ? 'bottom' : 'right'} className="p-0 w-72">
+							<TrashBox />
+						</PopoverContent>
+					</Popover>
+				</div>
+				<div
+					onMouseDown={handleMouseDown}
+					onClick={resetWidth}
+					className="top-0 right-0 absolute bg-primary/10 opacity-0 group-hover/sidebar:opacity-100 w-1 h-full transition cursor-ew-resize"
+				/>
+			</aside>
+			<div
+				ref={navbarRef}
+				className={cn(
+					'absolute top-0 z-[99999] left-60 w-[calc(100%-240px)]',
+					isResetting && 'transition-all ease-in-out duration-300',
+					isMobile && 'left-0 W-FULL'
+				)}>
+				{!!params.documentId ?
+					<Navbar isCollapsed={isCollapsed} onResetWidth={resetWidth} />
+				:	<nav className="bg-transparent px-3 py-2 w-full">
+						{isCollapsed && <MenuIcon role="button" onClick={resetWidth} className="w-6 h-6 text-muted-foreground" />}
+					</nav>
+				}
+			</div>
+		</>
+	);
 };
--- components/search-command.tsx
+++ components/search-command.tsx
@@ -1,77 +1,65 @@
-"use client";
+'use client';
 
-import {
-  CommandDialog,
-  CommandEmpty,
-  CommandGroup,
-  CommandInput,
-  CommandItem,
-  CommandList,
-} from "@/components/ui/command";
-import { api } from "@/convex/_generated/api";
-import { useSearch } from "@/hooks/use-search";
-import { useUser } from "@clerk/clerk-react";
-import { useQuery } from "convex/react";
-import { File } from "lucide-react";
-import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import { CommandDialog, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList } from '@/components/ui/command';
+import { api } from '@/convex/_generated/api';
+import { useSearch } from '@/hooks/use-search';
+import { useUser } from '@clerk/clerk-react';
+import { useQuery } from 'convex/react';
+import { File } from 'lucide-react';
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
 
 export const SearchCommand = () => {
-  const { user } = useUser();
-  const router = useRouter();
-  const documents = useQuery(api.documents.getSearch);
-  const [isMounted, setIsMounted] = useState<boolean>(false);
+	const { user } = useUser();
+	const router = useRouter();
+	const documents = useQuery(api.documents.getSearch);
+	const [isMounted, setIsMounted] = useState<boolean>(false);
 
-  const toggle = useSearch((state) => state.toggle);
-  const isOpen = useSearch((state) => state.isOpen);
-  const onClose = useSearch((state) => state.onClose);
+	const toggle = useSearch((state) => state.toggle);
+	const isOpen = useSearch((state) => state.isOpen);
+	const onClose = useSearch((state) => state.onClose);
 
-  useEffect(() => {
-    setIsMounted(true);
-  }, []);
+	useEffect(() => {
+		setIsMounted(true);
+	}, []);
 
-  useEffect(() => {
-    const down = (e: KeyboardEvent) => {
-      if (e.key === "k" && (e.metaKey || e.ctrlKey)) {
-        e.preventDefault();
-        toggle();
-      }
-    };
+	useEffect(() => {
+		const down = (e: KeyboardEvent) => {
+			if (e.key === 'k' && (e.metaKey || e.ctrlKey)) {
+				e.preventDefault();
+				toggle();
+			}
+		};
 
-    document.addEventListener("keydown", down);
-    return () => document.removeEventListener("keydown", down);
-  }, [toggle]);
+		document.addEventListener('keydown', down);
+		return () => document.removeEventListener('keydown', down);
+	}, [toggle]);
 
-  const onSelect = (id: string) => {
-    router.push(`/documents/${id}`);
-    onClose();
-  };
+	const onSelect = (id: string) => {
+		router.push(`/documents/${id}`);
+		onClose();
+	};
 
-  if (!isMounted) {
-    return null;
-  }
+	if (!isMounted) {
+		return null;
+	}
 
-  return (
-    <CommandDialog open={isOpen} onOpenChange={onClose}>
-      <CommandInput placeholder={`Search ${user?.fullName}'s Jotion`} />
-      <CommandList>
-        <CommandEmpty>No results found.</CommandEmpty>
-        <CommandGroup heading="Documents">
-          {documents?.map((document) => (
-            <CommandItem
-              key={document._id}
-              value={`${document._id}-${document.title}`}
-              title={document.title}
-              onSelect={onSelect}
-            >
-              {document.icon ?
-                <p className="mr-2 text-[18px]">{document.icon}</p>
-              : <File className="mr-2 size-4" />}
-              <span>{document.title}</span>
-            </CommandItem>
-          ))}
-        </CommandGroup>
-      </CommandList>
-    </CommandDialog>
-  );
+	return (
+		<CommandDialog open={isOpen} onOpenChange={onClose}>
+			<CommandInput placeholder={`Search ${user?.fullName}'s Jotion`} />
+			<CommandList>
+				<CommandEmpty>No results found.</CommandEmpty>
+				<CommandGroup heading="Documents">
+					{documents?.map((document) => (
+						<CommandItem key={document._id} value={`${document._id}`} title={document.title} onSelect={onSelect}>
+							{document.icon ?
+								<p className="mr-2 text-[18px]">{document.icon}</p>
+							:	<File className="mr-2 size-4" />}
+							<span>{document.title}</span>
+						</CommandItem>
+					))}
+				</CommandGroup>
+			</CommandList>
+		</CommandDialog>
+	);
 };
