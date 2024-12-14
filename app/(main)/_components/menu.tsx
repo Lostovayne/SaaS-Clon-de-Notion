1811abec5646cb311a348899bb801e8a49a4d3f3
Lostovayne <moises.barrera.dev@gmail.com>
2024-12-14 18:08:35 -0300

    feat: Agrega opciones al menu del costado derecho

--- app/(main)/_components/menu.tsx
+++ app/(main)/_components/menu.tsx
@@ -1,7 +1,17 @@
+import { Button } from '@/components/ui/button';
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu';
+import { Skeleton } from '@/components/ui/skeleton';
 import { api } from '@/convex/_generated/api';
 import type { Id } from '@/convex/_generated/dataModel';
 import { useUser } from '@clerk/clerk-react';
 import { useMutation } from 'convex/react';
+import { MoreHorizontal, Trash } from 'lucide-react';
 import { useRouter } from 'next/navigation';
 import { FC, ReactElement } from 'react';
 import { toast } from 'sonner';
@@ -10,7 +20,7 @@ interface MenuProps {
 	documentId: Id<'documents'>;
 }
 
-const Menu: FC<MenuProps> = ({ documentId }): ReactElement => {
+const Menu = ({ documentId }: MenuProps): ReactElement => {
 	const router = useRouter();
 	const { user } = useUser();
 	const archive = useMutation(api.documents.archive);
@@ -27,7 +37,27 @@ const Menu: FC<MenuProps> = ({ documentId }): ReactElement => {
 		router.push('/documents');
 	};
 
-	return <div>Menu</div>;
+	return (
+		<DropdownMenu>
+			<DropdownMenuTrigger asChild>
+				<Button size={'sm'} variant={'ghost'}>
+					<MoreHorizontal className={'size-4'} />
+				</Button>
+			</DropdownMenuTrigger>
+			<DropdownMenuContent className={'w-60'} align={'end'} alignOffset={8} forceMount>
+				<DropdownMenuItem onClick={onArchive}>
+					<Trash className={'size-4 mr-2'} />
+					Delete
+				</DropdownMenuItem>
+				<DropdownMenuSeparator />
+				<div className={'text-xs text-muted-foreground p-2'}>Last edited by: {user?.fullName}</div>
+			</DropdownMenuContent>
+		</DropdownMenu>
+	);
+};
+
+Menu.Skeleton = function MenuSkeleton() {
+	return <Skeleton className={'h-8 w-10'} />;
 };
 
 export default Menu;
--- app/(main)/_components/navbar.tsx
+++ app/(main)/_components/navbar.tsx
@@ -21,8 +21,11 @@ const Navbar = ({ isCollapsed, onResetWidth }: NavbarProps): ReactElement => {
 
 	if (document === undefined)
 		return (
-			<nav className="bg-background dark:bg-[#1f1f1f] px-3 py-2 w-full flex items-center">
+			<nav className="bg-background dark:bg-[#1f1f1f] px-3 py-2 w-full flex items-center justify-between">
 				<Title.Skeleton />
+				<div className={'flex items-center gap-x-2'}>
+					<Menu.Skeleton />
+				</div>
 			</nav>
 		);
 	if (document === null) return <div>Document not found</div>;
