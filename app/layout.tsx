e1f040518000e70c16834b07779485a939a0b6ae
Lostovayne <moises.barrera.dev@gmail.com>
2024-12-18 04:00:48 -0300

    add metas

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,17 +25,39 @@ export const metadata: Metadata = {
 				href: '/logo-dark.svg'
 			}
 		]
+	},
+	openGraph: {
+		title: "Notion's Connected Workspace - The best way to work together",
+		description:
+			'Notion is a connected workspace that helps you work together. It is the best way to collaborate and get things done.',
+		images: [
+			{
+				url: '/og-image.png',
+				width: 1200,
+				height: 630,
+				alt: 'Notion'
+			}
+		],
+		siteName: 'Notion App - The best way to work together',
+		locale: 'en_US',
+		type: 'website',
+		countryName: 'US',
+		emails: ['Deuslovult@notion.cl']
 	}
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
-		<html lang="en" suppressHydrationWarning>
+		<html lang='en' suppressHydrationWarning>
 			<body className={inter.className}>
 				<ConvexClientProvider>
 					<EdgeStoreProvider>
-						<ThemeProvider attribute="class" defaultTheme="system" disableTransitionOnChange storageKey="jotion-theme-2">
-							<Toaster position="bottom-right" />
+						<ThemeProvider
+							attribute='class'
+							defaultTheme='system'
+							disableTransitionOnChange
+							storageKey='jotion-theme-2'>
+							<Toaster position='bottom-right' />
 							<ModalProvider />
 							{children}
 						</ThemeProvider>
