74d3cbddae4004019af568538cbd71f36478457a
Lostovayne <moises.barrera.dev@gmail.com>
2024-12-17 21:00:51 -0300

    Enhance project configuration and UI components
    - Updated .env.template to include Edge Store configuration.
    - Updated package.json dependencies for Clerk, Radix UI, and added Edge Store packages.
    - Refactored layout.tsx to include EdgeStoreProvider for improved state management.
    - Improved ModeToggle component for theme switching.
    - Enhanced CoverImageModal to support file uploads using Edge Store.
    
    These changes improve the overall functionality and maintainability of the application.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,49 +1,47 @@
-import { ConvexClientProvider } from "@/components/providers/convex-provider";
-import { ModalProvider } from "@/components/providers/modal-provider";
-import { ThemeProvider } from "@/components/providers/theme-provider";
-import { Toaster } from "@/components/ui/sonner";
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import { ConvexClientProvider } from '@/components/providers/convex-provider';
+import { ModalProvider } from '@/components/providers/modal-provider';
+import { ThemeProvider } from '@/components/providers/theme-provider';
+import { Toaster } from '@/components/ui/sonner';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+import { EdgeStoreProvider } from '@/lib/edgestore';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Notion App",
-  description: "The connected workpace where better faster work happens.",
-  icons: {
-    icon: [
-      {
-        media: "(prefers-color-scheme:light)",
-        url: "/logo.svg",
-        href: "/logo.svg",
-      },
-      {
-        media: "(prefers-color-scheme:dark)",
-        url: "/logo-dark.svg",
-        href: "/logo-dark.svg",
-      },
-    ],
-  },
+	title: 'Notion App',
+	description: 'The connected workpace where better faster work happens.',
+	icons: {
+		icon: [
+			{
+				media: '(prefers-color-scheme:light)',
+				url: '/logo.svg',
+				href: '/logo.svg'
+			},
+			{
+				media: '(prefers-color-scheme:dark)',
+				url: '/logo-dark.svg',
+				href: '/logo-dark.svg'
+			}
+		]
+	}
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en" suppressHydrationWarning>
-      <body className={inter.className}>
-        <ConvexClientProvider>
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            disableTransitionOnChange
-            storageKey="jotion-theme-2"
-          >
-            <Toaster position="bottom-right" />
-            <ModalProvider />
-            {children}
-          </ThemeProvider>
-        </ConvexClientProvider>
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en" suppressHydrationWarning>
+			<body className={inter.className}>
+				<ConvexClientProvider>
+					<EdgeStoreProvider>
+						<ThemeProvider attribute="class" defaultTheme="system" disableTransitionOnChange storageKey="jotion-theme-2">
+							<Toaster position="bottom-right" />
+							<ModalProvider />
+							{children}
+						</ThemeProvider>
+					</EdgeStoreProvider>
+				</ConvexClientProvider>
+			</body>
+		</html>
+	);
 }
--- components/modals/cover-image-modal.tsx
+++ components/modals/cover-image-modal.tsx
@@ -1,10 +1,36 @@
 'use client';
-
 import { Dialog, DialogContent, DialogHeader } from '@/components/ui/dialog';
+import { api } from '@/convex/_generated/api';
+import type { Id } from '@/convex/_generated/dataModel';
 import { useCoverImage } from '@/hooks/use-cover-image';
+import { useEdgeStore } from '@/lib/edgestore';
+import { useMutation } from 'convex/react';
+import { useParams } from 'next/navigation';
+import React from 'react';
+import { SingleImageDropzone } from '../single-image-dropzone';
 
 export const CoverImageModal = () => {
+	const update = useMutation(api.documents.update);
+	const params = useParams();
+	const [file, setFile] = React.useState<File>();
+	const [isSubmitting, setIsSubmitting] = React.useState(false);
+	const { edgestore } = useEdgeStore();
 	const coverImage = useCoverImage();
+
+	const onClose = () => {
+		setFile(undefined);
+		setIsSubmitting(false);
+		coverImage.onClose();
+	};
+
+	const onChange = async (file?: File) => {
+		if (file) {
+			setIsSubmitting(true);
+			setFile(file);
+			const res = await edgestore.publicFiles.upload({ file });
+			await update({ id: params.documentId as Id<'documents'>, coverImage: res.url });
+		}
+	};
 
 	return (
 		<Dialog open={coverImage.isOpen} onOpenChange={coverImage.onClose}>
@@ -12,7 +38,7 @@ export const CoverImageModal = () => {
 				<DialogHeader>
 					<h2 className={'text-center text-lg font-semibold'}>Cover Image</h2>
 				</DialogHeader>
-				<div>TODO: Imagen de portada</div>
+				<SingleImageDropzone onChange={onChange} disabled={isSubmitting} className={'w-full outline-none'} value={file} />
 			</DialogContent>
 		</Dialog>
 	);
