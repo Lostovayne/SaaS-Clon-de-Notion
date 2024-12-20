5ecc89693565af762cf86ed5f2d54f7fb0f94474
Lostovayne <moises.barrera.dev@gmail.com>
2024-12-20 02:29:50 -0300

    fix: fixed modal button

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -2,7 +2,7 @@
 
 import { Spinner } from "@/components/spinner";
 import { Button } from "@/components/ui/button";
-import { SignInButton } from "@clerk/nextjs";
+import { SignInButton, SignUpButton } from "@clerk/nextjs";
 import { useConvexAuth } from "convex/react";
 import { ArrowRight } from "lucide-react";
 import Link from "next/link";
@@ -36,12 +36,12 @@ const Heading = () => {
 			)}
 
 			{!isAuthenticated && !isLoading && (
-				<SignInButton mode="modal">
+				<SignUpButton mode="modal">
 					<Button variant="default" size="sm">
 						Create account
 						<ArrowRight className="h-4 w-4 ml-2" />
 					</Button>
-				</SignInButton>
+				</SignUpButton>
 			)}
 		</div>
 	);
